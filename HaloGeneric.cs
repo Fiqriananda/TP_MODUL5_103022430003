9dea6d37fadb7aa9253b7ebc767ffe279679c1ac
Fiqriananda <fiqrianandahakin@gmail.com>
2026-03-29 20:58:45 +0700

    Add Generic Class and Method

---
+++ HaloGeneric.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace TP_MODUL5_103022430003
+{
+    public class HaloGeneric
+    {
+        public void SapaUser(String X)
+        {
+            Console.Write("Halo User " + X);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TP_MODUL5_103022430003;
+
+HaloGeneric h = new HaloGeneric();
+h.SapaUser("Fiqri");
